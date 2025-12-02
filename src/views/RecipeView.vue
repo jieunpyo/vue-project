8d720dd8124b7eca74402d550b739f810d1c59aa
jieunpyo <jieunhye1121@naver.com>
2025-12-02 16:49:39 +0900

    1111

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -1,15 +1,28 @@
 <template>
     <div class="container">
         <div class="row">
-
+        총 <span style="color:green;font-size: 35px;">{{counts}}</span>개의 맛있는 레시피가 있습니다.
         </div>
         <div class="row" style="margin-top:10px ;">
-
+            <div class="col-md-3" v-for="(vo,index) in recipe_list" :key="index">
+                <div class="thumbnail">
+                <router-link to="/">
+                <img :src="vo.poster" style="width:240px;height: 150px">
+                <div class="caption">
+                    <p>{{vo.title}}</p>
+                </div>
+                </router-link>
+            </div>
+            </div>
         </div>
         <div class="row text-center" style="margin-top:10px ;">
-
+            <ul class="pagination">
+                <li v-if="startPage>1"><a class="a-link" @click="prev(startPage-1)">&lt;</a></li>
+                <li v-for="(i,index) in range(startPage,endPage)" :class="i===curpage?'active':''" :key="index"><a class="a-link" @click="pageChange(i)">{{i}}</a></li>
+                <li v-if="endPage<totalpage"><a class="a-link" @click="next(endPage+1)">&gt;</a></li>
+            </ul>
         </div>
-    </div>
+        </div>
 </template>
 <script>
 import axios from 'axios'
@@ -28,6 +41,11 @@ export default({
     mounted(){
         this.dataRecv()
     },
+    computed:{
+        counts(){
+            return this.count.toLocaleString()
+        }
+    },
     methods:{
         // axios.post('url',null,{name:'',subject:''...})
         dataRecv(){
@@ -37,7 +55,39 @@ export default({
                 }
             }).then(response=>{
                 console.log(response.data)
+                this.count=response.data.count
+                this.recipe_list=response.data.list
+                this.food_list=response.data.list
+                this.curpage=response.data.curpage
+                this.totalpage=response.data.totalpage
+                this.startPage=response.data.startPage
+                this.endPage=response.data.endPage
             })
+        },
+        range(start,end){
+            let arr=[]
+            let len=end-start
+            for(let i=0;i<=len;i++)
+            {
+                arr[i]=start
+                start++
+            }
+            return arr
+        },
+        prev(page)
+        {
+            this.curpage=page
+            this.dataRecv()
+        },
+        next(page)
+        {
+            this.curpage=page
+            this.dataRecv()
+        },
+        pageChange(page)
+        {
+            this.curpage=page
+            this.dataRecv()
         }
     }
 
